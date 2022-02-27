b6751c10cbf630350a672e54e9ea15d49a97fb18
Adan <agandarilla0502@gmail.com>
2022-02-26 21:09:31 -0600

    add turn system

--- gui.py
+++ gui.py
@@ -15,44 +15,65 @@ CLOCK = pygame.time.Clock()
 FPS = 60
 
 class Game:
-    def __init__(self):
+    def __init__(self, bot_first):
         self.running = True
+        self.black_turn = True
         self.board = LIGHT_THEME
         self.screen = pygame.display.set_mode((1000, 1060))
-        self.points = [[62+(49*x), 122+(49*y), 0]
+        self.points = [[62+(49*x), 122+(49*y), -1]
                        for x in range(19) for y in range(19)]
-        self.screen.blit(self.board, (0, 0))
         
     def nearest(self, pos):
         """find which point is closest to the mouse"""
         pos = list(pos)
         closest = self.points[0][:2]
         dist = math.dist(pos, closest)
-        for p in self.points:
+        for i in range(len(self.points)):
+            p = self.points[i]
             new_dist = math.dist(pos, p[:2])
-            if new_dist < dist and p[2] == 0:
+            if new_dist < dist:
                 dist = new_dist
-                closest = tuple(p[:2])
+                closest = i
         return closest
+
+    def place_piece(self, pos):
+        index = self.nearest(pos)
+        if self.points[index][2] == -1:
+            self.points[index][2] = 0 if self.black_turn else 1
+            self.black_turn = not self.black_turn        
 
     def check_input(self):
         """check for user input"""
+        pos = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if 1028 > pos[1] > 95 and 965 > pos[0] > 35:
+                    self.place_piece(pos)
+            # check for mouse click on buttons can go here maybe
+
 
     def draw_screen(self):
         """draw everything on the screen"""
+        # draw board
+        self.screen.blit(self.board, (0, 0))
 
-        pos = pygame.mouse.get_pos()
-        if 1028 > pos[1] > 95 and 965 > pos[0] > 35:
-            pygame.draw.circle(self.screen, BLACK, self.nearest(pos), 25)
+        # drawing buttons can go here
 
+        # draw pieces
+        for p in self.points:
+            if p[2] != -1:
+                color = BLACK if (p[2] == 0) else WHITE
+                pygame.draw.circle(self.screen, color, p[:2], 25)
+
+        # finally flip display
         pygame.display.flip()
+        
         
 
 # Run main game loop
-game = Game()
+game = Game(bot_first=False)
 while game.running:
     CLOCK.tick(FPS)
     game.check_input()
