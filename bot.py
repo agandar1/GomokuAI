e0de54ce6442a8a3738f14de6f64340894891b99
Adan <agandarilla0502@gmail.com>
2022-04-07 01:15:41 -0500

    big refactor

--- bot.py
+++ bot.py
@@ -30,20 +30,11 @@ class Bot:
 
     def find_monomials(self, spot):
         """find all monomials that correspond to a certain spot"""
-        monomials = []
-        for m in self.monomials:
-            if ([spot[0], spot[1]] in m):
-                monomials.append(m)
-        return monomials
+        return [m for m in self.monomials if spot in m]
 
     def spot_value(self, board, spot):
         """calculate the value of a single spot on the board"""
-        vals = []
-        for m in self.find_monomials(spot):
-            vals.append([board[x[0]][x[1]] for x in m])
-        for i in range(len(vals)):
-            vals[i] = np.prod(vals[i]) 
-        return sum(vals)
+        return sum([self.mono_value(board, m, False) for m in self.find_monomials(spot)])
 
     def network(self, board):
         """calculate values of all spots to create a score board"""
@@ -71,41 +62,29 @@ class Bot:
             outer.append(right)
         return outer
 
-    def is_open(self, board, monomial):
+    def is_closed(self, board, monomial):
         """check if a monomial is open on both sides"""
+        # if there is only one outer point, or one of them is taken, it is closed
         outer = self.get_outer(monomial)
-        # if there is only one outer point, it is closed
-        if (len(outer) != 2):
-            return False
-        for spot in outer:
-            # if one of the 2 outer points are taken, it is closed
-            if (board[spot[0]][spot[1]] != 1):
-                return False
-        # otherwise it is open
-        return True
+        return (len(outer) != 2 or not all([board[s[0]][s[1]] == 1 for s in outer]))
     
-    def mono_value(self, board, mono, for_bot):
+    def mono_value(self, board, mono, for_self):
         """get the value for a single monomial"""
-        # calculate value of the monomial
+        # calculate value of the monomial, closed monomials are worth 0
         val = np.prod([board[x[0]][x[1]] for x in mono])
-        # invalidate if it is closed, we don't care about those
-        closed = not self.is_open(board, mono)
-        if ((val == 8 or (for_bot and 4 <= val <= 8 )) and closed):
+        if ((val == 8 or (for_self and 4 <= val <= 8 )) and self.is_closed(board, mono)):
             val = 0
         return val
         
     def find_best_monos(self, board, e_board):
         """find greatest monomial for both players"""
-        bot_scores = []
-        enemy_scores = []
-        for m in self.monomials:
-            # make a list of all monomials and their values
-            bot_scores.append([m, self.mono_value(board, m, for_bot=True)])
-            enemy_scores.append([m, self.mono_value(e_board, m, for_bot=False)])
+        # make a list of all monomials and their values
+        bot_scores = [[m, self.mono_value(board, m, True)] for m in self.monomials]
+        enemy_scores = [[m, self.mono_value(e_board, m, False)] for m in self.monomials]
         # return the highest one for each player
-        best = [max(bot_scores, key=lambda x: x[1]),
-                max(enemy_scores, key=lambda x: x[1])]
-        return(best)
+        random.shuffle(bot_scores)
+        random.shuffle(enemy_scores)
+        return [max(bot_scores, key=lambda x: x[1]), max(enemy_scores, key=lambda x: x[1])]
 
     def gen_opponent_board(self, bot_board):
         """generate the enemy's board by flipping bot's board"""
@@ -115,20 +94,9 @@ class Bot:
     
     def highest_spot(self, board, scores, enemy_scores):
         """find bot's most valuable spot"""
-        best = 0
-        coords = []
-        # just a linear search to collect all highest points
-        for x in range(self.size):
-            for y in range(self.size):
-                if board[x][y] == 1:
-                    val = scores[x][y]
-                    if (val > best):
-                        best = val
-                        coords = [[x, y]]
-                    elif (val == best):
-                        coords.append([x, y])
-        # if there is a tie, pick worse one for opponent
-        return max(coords, key=lambda x: enemy_scores[x[0]][x[1]])
+        coords = [[x, y] for x in range(19) for y in range(19) if board[x][y] == 1]
+        random.shuffle(coords)
+        return max(coords, key=lambda x: scores[x[0]][x[1]])
 
     def block(self, board, scores, enemy_scores, monos):
         """find the best place to block a monomial"""
@@ -136,57 +104,49 @@ class Bot:
         # block closed 4, or try to block open 4
         if (val == 16):
             available = [c for c in coords if board[c[0]][c[1]] == 1]
+            random.shuffle(available)
             return max(available, key=lambda x: scores[x[0]][x[1]])
-
         # pick most beneficial spot to block open 3 or starting move
         elif (val == 8):
             taken = [c for c in coords if board[c[0]][c[1]] != 1]
             x, y, last_x, last_y = taken[0][0], taken[0][1], taken[-1][0], taken[-1][1]
             change_x, change_y = coords[1][0] - coords[0][0], coords[1][1] - coords[0][1]
-            
             # get available vertical spots
             if (change_x == 0):
                 available = [[x, y-1], [last_x, last_y+1]]
-                for i in range(len(taken)):
-                    if (board[x][y+i] == 1):
-                        available.append([x, y+i])
+                available += [[x, y+i] for i in range(len(taken)) if board[x][y+i] == 1]
             # get available horizontal spots
             elif (change_y == 0):
                 available = [[x-1, y], [last_x+1, last_y]]
-                for i in range(len(taken)):
-                    if (board[x+i][y] == 1):
-                        available.append([x+i, y])
+                available += [[x+i, y] for i in range(len(taken)) if board[x+i][y] == 1]
             # get available negative slope diagonal spots
             elif (change_x > 0 and change_y > 0):
                 available = [[x-1, y-1], [last_x+1, last_y+1]]
-                for i in range(len(taken)):
-                    if (board[x+i][y+i] == 1):
-                        available.append([x+i, y+i])
+                available += [[x+i, y+i] for i in range(len(taken)) if board[x+i][y+i] == 1]
             # get available positive slope diagonal spots
             else:
                 available = [[x+1, y-1], [last_x-1, last_y+1]]
-                for i in range(len(taken)):
-                    if (board[x-i][y+i] == 1):
-                        available.append([x-i, y+i])
+                available += [[x-i, y+i] for i in range(len(taken)) if board[x-i][y+i] == 1]
             # choose the one best for the bot
+            random.shuffle(available)
             move = max(available, key=lambda x: scores[x[0]][x[1]])
             return move
-
         # choose a spot close to the opponent's opening
         elif (val == 2):
-            available = [c for c in coords if board[c[0]][c[1]] == 1]
+            available = [[x, y] for x in range(19) for y in range(19) if board[x][y] == 1]
+            random.shuffle(available)
             return max(available, key=lambda x: enemy_scores[x[0]][x[1]])
-
         # otherwise just try to build
         else:
             return self.build(board, scores, enemy_scores, monos[0])
             
     def build(self, board, scores, enemy_scores, best_mono):
         """pick a spot to try to win"""
-        coords, val = best_mono[0], best_mono[1]
+        coords, val = best_mono
         # immediately win if we have 4 in a row or pick best spot in open 3
         if (val >= 8):
             available = [c for c in coords if board[c[0]][c[1]] == 1]
+            random.shuffle(available)
             return max(available, key=lambda x: scores[x[0]][x[1]])
         # otherwise just get most valuable spot
         else:
@@ -213,8 +173,7 @@ class Bot:
         best_monos = self.find_best_monos(self.board, enemy_board)
         if (best_monos[1][1] > best_monos[0][1] and best_monos[1][1] != 4):
             # block if enemy is ahead
-            best_move = self.block(self.board, self.score_board,
-                                   enemy_scores, best_monos)
+            best_move = self.block(self.board, self.score_board, enemy_scores, best_monos)
         else:
             # ignore enemy and build if bot is ahead
             best_move = self.build(self.board, self.score_board, enemy_scores, best_monos[0]) 
